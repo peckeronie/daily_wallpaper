be843b3ccaa77605394f4220a0314c3d0eff36ef
Peiqi Chen <peiqichen0102@gmail.com>
2020-06-24 16:18:55 -0400

    Corrected syntax of arguments for schedule

--- setwallpaper.py
+++ setwallpaper.py
@@ -11,23 +11,23 @@ def getImage(website):
     page.close()
     element = html.find("meta",  property="og:image")
     link = element["content"]
-    return link 
-    
+    return link
+
 def saveImage(link, folder):
     file_name = link.split("/")[-1]
     image_request = requests.get(link)
     if (os.path.isdir(folder) == False): #make folder to hold wallpaper images
         os.mkdir(folder)
     file_path = os.path.join(folder, file_name)
-    print(file_path) 
+    print(file_path)
     open(file_path, 'wb').write(image_request.content) #save the image into the folder
     image_request.close()
     print("image saved")
     return file_path
 
-    
+
 def setWallpaper(file_path, directory):
-    
+
     pathname = os.fsdecode(file_path)
     print("Setting wallpaper...pathname:")
     print(pathname)
@@ -44,13 +44,13 @@ def setWallpaper(file_path, directory):
             print("Image removed")
         else:
             print("Image is the same")
-            
-    abs_path = os.path.abspath(file_path) #get absolute path for setting wallpaper   
+
+    abs_path = os.path.abspath(file_path) #get absolute path for setting wallpaper
     ctypes.windll.user32.SystemParametersInfoW(20, 0, abs_path, 0)
     print(" ")
     print("Wallpaper set")
 
-    
+
 def setTags(tag_list):
     str1 = r"https://danbooru.donmai.us/posts/random?tags="
     for tag in tag_list:
@@ -58,9 +58,9 @@ def setTags(tag_list):
         str1 = str1 + tag + "+"
     str1 = str1[:-1]
     return str1
-    
+
 def main(list1,foldername):
-    
+
     set_url = setTags(list1)
     pic_url = getImage(set_url)
     path = saveImage(pic_url, foldername)
@@ -70,7 +70,8 @@ def main(list1,foldername):
 
 some_tags = ["scenery","cherry blossoms"]
 foldername = "wallpaperfolder"
-schedule.every().day.at("05:00").do(main(some_tags, foldername))
+#schedule.every().day.at("05:00").do(main(some_tags, foldername))
+schedule.every().day.at("05:00").do(main, list1=some_tags, foldername=folder1)
 
 while True:
     schedule.run_pending()
